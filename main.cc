f72ea4303229be2d3d0d24862c9c2cb531edfd98
Josh Marusek <jm553020@ohio.edu>
2022-04-11 22:24:24 -0400

    finish part 1

--- main.cc
+++ main.cc
@@ -15,9 +15,17 @@ int main() {
     convert_to_base(num, 3);
     cout << "\nBase 7: ";
     convert_to_base(num, 7);
+    cout << '\n';
 }
 
 
 void convert_to_base(int n, int base) {
-    
+    // base case
+    if (n < base) {
+        cout << n;
+    }
+    else {
+        convert_to_base(n / base, base);  // variant statmement
+        cout << n % base;
+    }
 }
